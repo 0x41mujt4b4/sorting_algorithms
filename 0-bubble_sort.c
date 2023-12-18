7dfbd5ec6127cac603ec67b2a71c942b92595de3
0x41mujt4b4 <almujtaba007@gmail.com>
2023-12-18 05:17:15 -0800

    update bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdbool.h>
 
 /**
  * bubble_sort - sort an array
@@ -10,9 +11,11 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, temp;
+	bool swapped;
 
 	for (i = 0; i < size; i++)
 	{
+		swapped = false;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -20,9 +23,11 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				swapped = true;
+				print_array(array, size);
 			}
-			print_array(array, size);
-
 		}
+		if (swapped == false)
+			break;
 	}
 }
